096d901b8b7e7d876da7d2d4a8187ec2b55e8549
Maryam <maryam.eloraby@gmail.com>
2021-11-07 13:56:39 +0200

    issue: flight date

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const {MongoURI} = require("./config/keys");
+const {MongoURI} = require("../config/keys");
 const cors = require ("cors");
 
 
--- src/client/src/App.js
+++ src/client/src/App.js
@@ -3,7 +3,6 @@ import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes , Route } from 'react-router-dom'
 // import Header from './components/Header'
 // import { BrowserRouter as Router, Route } from 'react-router-dom';
-import DataTable from './components/DataGrid';
 import EnhancedTable from './components/EnhancedGrid.js';
 import ViewFlights from './components/ViewFlights.js';
 
