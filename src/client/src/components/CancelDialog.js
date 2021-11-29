1f07cedced44919f739c2152a707e316c9c72d64
Hadeer <hadeerelhussen1111@gmail.com>
2021-11-29 16:52:00 +0200

    Commit 1

--- src/client/src/components/CancelDialog.js
+++ src/client/src/components/CancelDialog.js
@@ -1,13 +1,15 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 import axios from "axios";
 
-export default function CancelDialog({reser}) {
+var canceledFlight;
+
+export default function CancelDialog({ reser }) {
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -19,18 +21,51 @@ export default function CancelDialog({reser}) {
   };
 
   const cancelFlight = () => {
-      axios
-      .delete('http://localhost:8000/reservations/' + reser)
-      .then(res => {
+    axios
+      .delete("http://localhost:8000/reservations/" + reser)
+      .then((res) => {
         console.log("success");
-         window.location.reload(false);
+        window.location.reload(false);
         setOpen(false);
-
       })
-      .catch(err => {
+      .catch((err) => {
         console.log("Error in FlightDelete!");
-      }
-      )};
+      });
+  };
+
+  // const getReservation = () => {
+  //   axios
+  //     .get(`http://localhost:8000/reservations/all-reservations`)
+  //     .then((res) => {
+  //       var flight="";
+  //       var refundAmount =0;
+  //       var cabin ="";
+  //       var arr = res.data;
+  //        var n = Object.keys(arr).length;
+  //        for (let i=0; i<n; i++){
+  //            if(arr[i]._id === reser){
+  //              flight = arr[i].flight;
+  //              refundAmount = arr[i].noOfPassengers;
+  //              cabin = arr[i].choosenCabin;
+  //            }
+  //           }
+  //             axios
+  //             .get("http://localhost:8000/flights/"+flight)
+  //             .then((res) => {
+  //               if(cabin === "Economy")
+  //               refundAmount = refundAmount*res.data.eprice;
+  //               if(cabin === "Business")
+  //               refundAmount = refundAmount*res.data.bprice;
+  //               if(cabin === "First")
+  //               refundAmount = refundAmount*res.data.fprice;
+  //               var info =`you have canceled this flight ${res.data}. Your refund amount is ${refundAmount}`;
+  //               axios
+  //               .post("http://localhost:8000/reservations/send",info)
+  //               .then((res) => {
+  //                   cancelFlight = res.data;
+  //               });
+  //             });
+  //           };
 
   return (
     <div>
@@ -48,12 +83,14 @@ export default function CancelDialog({reser}) {
         </DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
-          Upon clicking cancel, this flight will be permenantly cancelled.
+            Upon clicking cancel, this flight will be permenantly cancelled.
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button align = "left" onClick={handleClose}>Back</Button>
-          <Button color="error" align = "right" onClick={cancelFlight} autoFocus>
+          <Button align="left" onClick={handleClose}>
+            Back
+          </Button>
+          <Button color="error" align="right" onClick={cancelFlight} autoFocus>
             Cancel
           </Button>
         </DialogActions>
