13aee917b2047366b2e08202ecd93919241a99c4
Maryam <maryam.eloraby@gmail.com>
2021-12-01 19:22:03 +0200

    Fixing routers

--- src/Routes/reservationController.js
+++ src/Routes/reservationController.js
@@ -71,9 +71,27 @@ router.post("/send", async (req, res) => {
       rejectUnauthorized: false,
     },
   });
+  //send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"NodeMailar" <winter21team@gmail.com>', // sender address
+    to: "hadeerelhussen1111@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: output, // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+});
 
   router.get("/allreservations", async (req, res) => {
-    console.log(req.body);
+
+    console.log("Hiiiiiiiiiii");
+    //console.log(req.body);
     Reservation.find()
       .then((result) => {
         res.send(result);
@@ -126,23 +144,5 @@ router.post("/send", async (req, res) => {
         res.status(400).send("Error fetching reservation!");
       });
   });
-
-  module.exports = router;
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"NodeMailar" <winter21team@gmail.com>', // sender address
-    to: "hadeerelhussen1111@gmail.com", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: output, // html body
-  });
-
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-});
 
 module.exports = router;
