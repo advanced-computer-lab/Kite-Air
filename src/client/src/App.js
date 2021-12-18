6c15488160a06af1af385de43d67f7bea334aff8
Hadeer <hadeerelhussen1111@gmail.com>
2021-12-19 01:07:03 +0200

    Commit 1

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -10,20 +10,35 @@ import CreateFlight from "./components/CreateFlight.js";
 import Search from "./components/Search.js";
 import ProfilePage from "./components/ProfilePage.js";
 import MyBookings from "./components/MyBookings.js";
+import Payment from "./components/Payment.js";
 import SignIn from "./components/SignIn";
 import Test from "./components/Test";
 import CollapsibleTable from "./components/CollapsibleTable";
 import SeatsPickermain from "./components/SeatsPickermain";
-
+import { Elements } from "@stripe/react-stripe-js";
 import { useEffect, useState } from "react";
-
-
+import { loadStripe } from "@stripe/stripe-js";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+import { publishableKeyGet } from "./functions";
 // import { useContext } from "react";
 // import { UserContext } from "./context/index.js";
 
 const App = () => {
   const [user, setUser] = useState({});
   // const [state, setState] = useContext(UserContext);
+  //const stripePromise = loadStripe("STRIPE_PUBLISHABLE_API_KEY");
+  const [stripePromise, setStripePromise] = useState(null);
+
+  useEffect(() => {
+    const retrievePublishableKey = async () => {
+      // const publishableKey = await publishableKeyGet();
+      const stripe = loadStripe("pk_test_nqH70Fb8FmabuVsU5kp4gpYf00XGNeVxyf");
+      setStripePromise(stripe);
+    };
+    retrievePublishableKey();
+  }, []);
 
   return (
     <UserProvider>
@@ -40,6 +55,22 @@ const App = () => {
             <Route path="ProfilePage" element={<ProfilePage />} />
             <Route path="mybookings" element={<MyBookings />} />
             <Route path="login" element={<SignIn />} />
+            <Route
+              path="/Payment"
+              element={
+                <Box>
+                  <Container maxWidth="md">
+                    <Paper elevation={5}>
+                      {stripePromise ? (
+                        <Elements stripe={stripePromise}>
+                          <Payment />
+                        </Elements>
+                      ) : null}
+                    </Paper>
+                  </Container>
+                </Box>
+              }
+            />
             <Route
               path="/userReservations"
               element={<CollapsibleTable />}
