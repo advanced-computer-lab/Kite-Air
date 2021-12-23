47fefdc6a0ffddab429de97518a1b1e98c6e7e2e
Hadeer <hadeerelhussen1111@gmail.com>
2021-12-23 18:54:01 +0200

    Commit 2

--- src/client/src/components/EmailButton.js
+++ src/client/src/components/EmailButton.js
@@ -25,21 +25,54 @@ export default function EmailButton({ entry }) {
     );
 
     var info = ` 
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <meta charset="UTF-8">
+    <style>
+    table, th, td {
+      border: 1px solid black;
+      border-collapse: collapse;
+    }
+
+    td {
+      background-color: #b7d7e8;
+    }
+    tr {
+      border-bottom: 1px solid #ddd;
+    }
+    th {
+      text-align: left;
+      background-color: #87bdd8;
+    }
+    </style>
+    </head>
+    <body>
     <p>Hello ${state.user.FirstName},</p>
+    <p>This email is sent upon your request. Here's your flight reservation.</p>
     <br/>
-    Flight Number: ${entry[2]}
-    <br/>
-                 From: ${entry[3]}
-                 <br/>
-                 To: ${entry[4]}
-                 <br/>
-                 FlightDate: ${entry[5]}
-                 <br/>
-                 DepartureTime: ${entry[6]},
-                 <br/>
-                 ArrivalTime: ${entry[7]},
-                 <br/>
-                 KITE AIR :)
+    <table>
+  <tr>
+    <th> Flight Number  </th>
+    <th> From  </th>
+    <th> To  </th>
+    <th> FlightDate  </th>
+    <th> DepartureTime  </th>
+    <th> ArrivalTime  </th>
+  </tr>
+  <tr>
+    <td> ${entry[2]}  </td>
+    <td> ${entry[3]}  </td>
+    <td> ${entry[4]}  </td>
+    <td> ${entry[5]}  </td>
+    <td> ${entry[6]}  </td>
+    <td> ${entry[7]}  </td>
+  </tr>
+</table>
+<br/>
+</body>
+</html>
+<p>KITE AIR &#9992;</p>
       `;
 
     var email = `${state.user.Email}`;
@@ -69,3 +102,16 @@ export default function EmailButton({ entry }) {
     </Button>
   );
 }
+
+// <br/>    Flight Number: ${entry[2]}
+// <br/>
+//              From: ${entry[3]}
+//              <br/>
+//              To: ${entry[4]}
+//              <br/>
+//              FlightDate: ${entry[5]}
+//              <br/>
+//              DepartureTime: ${entry[6]},
+//              <br/>
+//              ArrivalTime: ${entry[7]},
+//              <br/>
