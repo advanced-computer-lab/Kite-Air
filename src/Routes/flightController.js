c6976bb328bc554d1ae3dd6db6f2e95f67a7a979
Maryam <maryam.eloraby@gmail.com>
2021-12-24 05:16:22 +0200

    Revert "res.send error fixed"
    This reverts commit 3e93ba972b9f0c7b740b6a725aa183c4ad466c64.

--- src/Routes/flightController.js
+++ src/Routes/flightController.js
@@ -105,10 +105,9 @@ router.put("/:id", async (req, res) => {
   console.log(req.params.id);
   await Flight.findByIdAndUpdate(req.params.id, req.body)
     .then((result) => {
+      res.status(200).send("User updated ");
+
       console.log("The User is Updated successfully !");
-
-      return   res.status(200).send("User updated ");
-
     })
     .catch((err) => {
       console.log(err);
@@ -185,7 +184,7 @@ router.post("/search-m2", async (req, res) => {
   if (Object.keys(req.body).length != 0) {
     await Flight.find(req.body)
       .then((result) => {
-        return res.send(result);
+        res.send(result);
       })
       .catch((err) => {
         console.log(err);
@@ -196,10 +195,9 @@ router.post("/search-m2", async (req, res) => {
 router.route("/:id").delete(async (req, res) => {
   await Flight.findByIdAndDelete(req.params.id)
     .then((result) => {
+      res.status(200).send("Flight deleted ");
+
       console.log("The Flight is deleted successfully !");
-
-      return res.status(200).send("Flight deleted ");
-
     })
     .catch((err) => {
       console.log(err);
